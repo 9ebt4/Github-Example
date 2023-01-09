51cd3d7e735726209520964c586915e343203588
ethan <ethanbjansen@gmail.com>
2023-01-09 09:44:59 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//NULL
+string myName = null;
+Console.WriteLine(myName);
+string friend = "Family";
+
+if (friend == "Family")
+{
+    myName = "Justin";
+}
+else if (friend == "Josh Carolin")
+{
+    myName = "Jeo";
+}
+Console.WriteLine(myName);
