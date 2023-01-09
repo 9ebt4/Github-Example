e7655be5188707a1b40f0110ace703259c546251
ethan <ethanbjansen@gmail.com>
2023-01-09 10:06:05 -0600

    added comments

--- Program.cs
+++ Program.cs
@@ -2,16 +2,9 @@
 Console.WriteLine("Hello, World!");
 
 //NULL
-string myName = null;
-Console.WriteLine(myName);
-string friend = "Family";
 
-if (friend == "Family")
-{
-    myName = "Justin";
-}
-else if (friend == "Josh Carolin")
-{
-    myName = "Jeo";
-}
-Console.WriteLine(myName);
+/* making changes, making changes.
+ * 
+ * 
+ * sdfjlaskdf
+ */
